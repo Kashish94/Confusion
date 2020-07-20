f9e2a36d3d0369d07ae37f75e893c20bd36f5229
Kashish94 <kashishsinghlive@gmail.com>
2020-07-21 01:00:59 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -15,6 +15,7 @@ import {Link} from 'react-router-dom';
 import {Control, Errors, LocalForm} from "react-redux-form";
 import FormGroup from "reactstrap/lib/FormGroup";
 import { addComment } from '../redux/ActionCreators';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({dish}) {
     return (
@@ -56,10 +57,25 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-
-
-    const dish = props.dish;
-    if (dish != null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
 
         return (
             <div className="container">
